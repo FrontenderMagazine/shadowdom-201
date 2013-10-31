4e0e82afbac0578a8bfd798c2ef441370374980f
SilentImp <thesilentimp@gmail.com>
2013-10-31 13:59:10 +0200

    article

--- js/example-4.js
+++ js/example-4.js
@@ -1,19 +1,22 @@
 (function() {
-var container = document.querySelector('#style-ex-inheritance');
-var root = container.createShadowRoot();
-root.applyAuthorStyles = true;
-//root.resetStyleInheritance = false;
-root.innerHTML = '<style>b{color: red;}</style><b>Shadow DOM Title</b><content select="b"></content>';
+  
+  var container = document.querySelector('#style-ex-inheritance');
+  var root = container.createShadowRoot();
+  
+  root.applyAuthorStyles = true;
+  //root.resetStyleInheritance = false;
+  root.innerHTML = '<style>b{color: red;}</style><b>Заголовок, принадлежащий теневому дереву</b><content select="b"></content>';
 
-document.querySelector('#demo-applyAuthorStyles').addEventListener('click', function(e) {
-  root.applyAuthorStyles = !root.applyAuthorStyles;
-  e.target.textContent = 'applyAuthorStyles=' + root.applyAuthorStyles;
-  document.querySelector('#code-applyAuthorStyles').textContent = root.applyAuthorStyles;
-});
-document.querySelector('#demo-resetStyleInheritance').addEventListener('click', function(e) {
-  root.resetStyleInheritance = !root.resetStyleInheritance;
-  e.target.textContent = 'resetStyleInheritance=' + root.resetStyleInheritance;
-  document.querySelector('#code-resetStyleInheritance').textContent = root.resetStyleInheritance;
-});
+  document.querySelector('#demo-applyAuthorStyles').addEventListener('click', function(e) {
+    root.applyAuthorStyles = !root.applyAuthorStyles;
+    e.target.textContent = 'applyAuthorStyles=' + root.applyAuthorStyles;
+    document.querySelector('#code-applyAuthorStyles').textContent = root.applyAuthorStyles;
+  });
+
+  document.querySelector('#demo-resetStyleInheritance').addEventListener('click', function(e) {
+    root.resetStyleInheritance = !root.resetStyleInheritance;
+    e.target.textContent = 'resetStyleInheritance=' + root.resetStyleInheritance;
+    document.querySelector('#code-resetStyleInheritance').textContent = root.resetStyleInheritance;
+  });
 
 })();
